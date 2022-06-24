5e5a3bc7307d53079a2a1ab35d567df8920f82df
Chinaelojo <okparachinealo@gmail.com>
2022-06-24 02:14:48 +0100

    third commit

---
+++ Java-assignment/intro.js
@@ -0,0 +1,7 @@
+alert('introduction with zuri')
+
+let name = 'Okpara Chinaelo Josephine'
+let height = '173cm'
+let country = 'Nigeria'
+
+document.write(name + "<br>" + height + "<br>" + country);
